ac64a62f10cbe71e8b50a39b8d05e9e0694068de
orderly-guoqing <guoqing@orderly.network>
2026-09-06 13:49:54 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "DOLA DOLA DOLA DOLA DOLA",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Markets,Swap,Portfolio,Leaderboard,Points,Rewards,Vaults",
+  "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Leaderboard,Points,Rewards,Vaults",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
